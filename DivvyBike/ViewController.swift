9540b9fe0e5aaa44eeb3b797c9d0100e2b0a8a1e
Olivia Marunde <omarunde8029@jh-mba158-10.local>
2017-09-08 12:27:39 -0500

    Initial Commit

---
+++ DivvyBike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DivvyBike
+//
+//  Created by Olivia Marunde on 9/8/17.
+//  Copyright © 2017 Olivia Marunde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
